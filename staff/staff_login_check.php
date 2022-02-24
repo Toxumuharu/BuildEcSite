62ae48864241c73775585c7213b1178e519fcccf
Tokawata <tokawata@microsoft.com>
2022-02-24 17:41:35 +0900

    add update

--- staff/staff_login_check.php
+++ staff/staff_login_check.php
@@ -38,9 +38,9 @@ if ($link) { // success to login database
     $sql = "SELECT name FROM mst_staff WHERE code = '$code' AND password = '$pass'";
     //$sql = "SELECT code FROM mst_staff WHERE name = '$code' AND password = '$pass'"; // if name and code
 
-    // show_sql($sql);
+    show_sql($sql);
     $result = mysqli_query($link, $sql);
-    // show_result($result);
+    show_result($result);
 
     if(empty($result["name"]) === true) {
     // if(empty($result["code"]) === true) { // if name and code
