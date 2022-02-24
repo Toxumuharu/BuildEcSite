8e22afba46b7dab5957717f73d68928979fcf614
Tokawata <tokawata@microsoft.com>
2022-02-24 15:34:15 +0900

    add update

--- staff/staff_login_check.php
+++ staff/staff_login_check.php
@@ -30,47 +30,39 @@ $link = mysqli_connect($_["datasource"], $_["userid"], $_["password"], "shop");
     
 if ($link) { // success to login database
     $db_selected = mysqli_select_db($link, "shop");
-    $sql = "SELECT code FROM mst_staff WHERE name = '$code' AND password = '$pass'";
-    echo nl2br("{$sql}");
+    $sql = "SELECT name FROM mst_staff WHERE code = '$code' AND password = '$pass'";
+    //$sql = "SELECT code FROM mst_staff WHERE name = '$code' AND password = '$pass'"; // if name and code
+    // echo nl2br("{$sql}\n"); // show sql
     $result = mysqli_query($link, $sql);
     
-    // if ($result)
-    // {
-    // // Fetch one and one row
-    // while ($row=mysqli_fetch_row($result))
-    //     {
-    //     printf ("%s (%s)\n",$row[0],$row[1]);
-    //     }
-    // }
-
-    echo nl2br("result:");
-    $result = mysqli_fetch_assoc($result);
-    print_r($result);
+    echo nl2br("result: ");
+    print_r(mysqli_fetch_assoc($result));
     echo nl2br("\n");
 
-
-
-        if(empty($result["code"]) === true) {
-            print "入力が間違っています。<br><br>";
-            print "<a href='staff_login.html'>戻る</a>";
-            exit();
-        } else {
-            echo nl2br("session_start()\n");
-            // session_start();
-            $_SESSION["login"] = 1;
-            $_SESSION["name"] = $result["name"];
-            $_SESSION["code"] = $code;
-            // header("Location:staff_login_top.php");
-            exit();
-        }
+    if(empty($result["code"]) === true) {
+    // if(empty($result["code"]) === true) { // if name and code
+        print "入力が間違っています。<br>";
+        print "<a href='staff_login.html'>戻る</a>";
+        exit();
+    } else {
+        // echo nl2br("session_start()\n");
+        session_start();
+        $_SESSION["login"] = 1;
+        $_SESSION["name"] = $result["name"];
+        $_SESSION["code"] = $code;
+        // $_SESSION["name"] = $code; // if name and code
+        // $_SESSION["code"] = $result["code"]; // if name and code
+        header("Location:staff_login_top.php");
+        exit();
+    }
     mysqli_close($link);
 }else{
     echo nl2br("Fail to login database");
     $link = mysqli_connect($_["datasource"], $_["userid"], $_["password"], "shop");
     mysqli_close($link);
 }
-}
-catch(Exception $e) {
+
+}catch(Exception $e) {
     print ($e);
     print "只今障害が発生しております。<br><br>";
     print "<a href='staff_login.html'>戻る</a>";
