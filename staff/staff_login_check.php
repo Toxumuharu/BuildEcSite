34870f9cb0a7ac98f91db0df12ea12eea6a55d2c
Tokawata <tokawata@microsoft.com>
2022-02-25 14:24:04 +0900

    add update

--- staff/staff_login_check.php
+++ staff/staff_login_check.php
@@ -44,6 +44,35 @@ $azure_mysql_connstr_match = preg_match(
     $_);
 $link = mysqli_connect($_["datasource"], $_["userid"], $_["password"], "shop");
     
+$dsn = "mysql:host={$_["datasource"]};dbname=shop;charset=utf8";
+$user = $_["userid"];
+$password = $_["password"];
+$dbh = new PDO($dsn, $user, $password);
+$dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "SELECT name FROM mst_staff WHERE code=? AND password=?";
+$stmt = $dbh -> prepare($sql);
+$data[] = $code;
+$data[] = $pass;
+$stmt -> execute($data);
+$dbh = null;
+$rec = $stmt -> fetch(PDO::FETCH_ASSOC);
+
+if(empty($rec["name"]) === true) {
+    print "入力が間違っています。<br><br>";
+    print "<a href='staff_login.html'>戻る</a>";
+    exit();
+} else {
+    echo nl2br("session_start();");
+
+    session_start();
+    $_SESSION["login"] = 1;
+    $_SESSION["name"] = $rec["name"];
+    $_SESSION["code"] = $code;
+    header("Location:staff_login_top.php");
+    exit();
+}
+
+    
 if ($link) { // success to login database
     $db_selected = mysqli_select_db($link, "shop");
     $sql = "SELECT name FROM mst_staff WHERE code = '$code' AND password = '$pass'";
@@ -52,7 +81,7 @@ if ($link) { // success to login database
     show_sql($sql);
     $result = mysqli_query($link, $sql);
     show_result($result);
-    $result_array = mysqli_fetch_assoc($result);
+    $result_array = mysqli_fetch_assoc($result);    
 
     if(empty($result_array["name"]) === true) {
     // if(empty($result["code"]) === true) { // if name and code
