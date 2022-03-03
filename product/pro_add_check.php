f1c4f3c86489165c53ae360981b6a217f2aa4184
Tokawata <tokawata@microsoft.com>
2022-03-03 17:02:27 +0900

    add update

--- product/pro_add_check.php
+++ product/pro_add_check.php
@@ -51,7 +51,7 @@ if(preg_match("/\A[0-9]+\z/", $price) === 0) {
 }
     
 if($gazou["size"] > 0) {
-    if($gazou["size"] > 2000000) {
+    if($gazou["size"] > 10000000) {
         print "ファイルのサイズが大きすぎます。<br><br>";
     } else {
         move_uploaded_file($gazou["tmp_name"],"./gazou/".$gazou["name"]);
@@ -71,7 +71,7 @@ if(mb_strlen($comments) > 100) {
     print "<br><br>";
 }
     
-if(empty($name) or preg_match("/\A[0-9]+\z/", $price) === 0 or $gazou["size"] > 2000000 or empty($comments) or mb_strlen($comments) > 100) {
+if(empty($name) or preg_match("/\A[0-9]+\z/", $price) === 0 or $gazou["size"] > 10000000 or empty($comments) or mb_strlen($comments) > 100) {
     print "<form>";
     print "<input type='button' onclick='history.back()' value='戻る'>";
     print "</form>";
