8b7e7789fb6f8a0977ea55b0c037f4b2bfdf7fcc
Tokawata <tokawata@microsoft.com>
2022-03-17 14:27:26 +0900

    add update

--- common/common.php
+++ common/common.php
@@ -17,4 +17,17 @@ function pulldown_cate() {
     print "</select>";
 }
 
+// function resize_image($original_image) {
+//     list($width, $hight) = getimagesize('test.jpg'); // 元の画像名を指定してサイズを取得
+//     $baseImage = imagecreatefromjpeg('test.jpg'); // 元の画像から新しい画像を作る準備
+//     $image = imagecreatetruecolor(100, 100); // サイズを指定して新しい画像のキャンバスを作成
+    
+//     // 画像のコピーと伸縮
+//     imagecopyresampled($image, $baseImage, 0, 0, 0, 0, 100, 100, $width, $hight);
+    
+//     // コピーした画像を出力する
+//     imagejpeg($image , 'new.jpg');
+//     return image;
+// }
+
 ?>
