d347f56a648bb6d3f70c3ec595272e2d27415f32
Tokawata <tokawata@microsoft.com>
2022-03-17 14:43:28 +0900

    add update

--- product/pro_disp.php
+++ product/pro_disp.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 session_regenerate_id(true);
 if(isset($_SESSION["login"]) === false) {
@@ -13,53 +14,55 @@ if(isset($_SESSION["login"]) === false) {
 
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>商品詳細</title>
-        <link rel="stylesheet" href="style.css">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>商品詳細</title>
+<link rel="stylesheet" href="style.css">
 </head>
-
+    
 <body>
-
+    
 <?php
-try{
-    $code = $_GET["code"];
+    try{
+    
+$code = $_GET["code"];
 
-    // azure database login
-    $azure_mysql_connstr = $_SERVER["MYSQLCONNSTR_localdb"];
-    $azure_mysql_connstr_match = preg_match(
-        "/".
-        "Database=(?<database>.+);".
-        "Data Source=(?<datasource>.+);".
-        "User Id=(?<userid>.+);".
-        "Password=(?<password>.+)".
-        "/u",
-        $azure_mysql_connstr,
-        $_);
+// azure database login
+$azure_mysql_connstr = $_SERVER["MYSQLCONNSTR_localdb"];
+$azure_mysql_connstr_match = preg_match(
+    "/".
+    "Database=(?<database>.+);".
+    "Data Source=(?<datasource>.+);".
+    "User Id=(?<userid>.+);".
+    "Password=(?<password>.+)".
+    "/u",
+    $azure_mysql_connstr,
+    $_);
+    
+$dsn = "mysql:host={$_["datasource"]};dbname=shop;charset=utf8";
+$user = $_["userid"];
+$password = $_["password"];
 
-    $dsn = "mysql:host={$_["datasource"]};dbname=shop;charset=utf8";
-    $user = $_["userid"];
-    $password = $_["password"];
-
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $sql = "SELECT category, code, name, price, gazou, explanation FROM mst_product WHERE code?";
-    $stmt = $dbh -> prepare($sql);
-    $data[] = $code;
-    $stmt -> execute($data);
-
-    $dbh = null;
-
-    $rec = $stmt -> fetch(PDO::FETCH_ASSOC);
+$dbh = new PDO($dsn, $user, $password);
+$dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+$sql = "SELECT category, code, name, price, gazou, explanation FROM mst_product WHERE code=?";
+$stmt = $dbh -> prepare($sql);
+$data[] = $code;
+$stmt -> execute($data);
+    
+$dbh = null;
+        
+$rec = $stmt -> fetch(PDO::FETCH_ASSOC);
+        
 }
 catch(Exception $e) {
     print "只今障害が発生しております。<br><br>";
-    print "<a href='../staff/staff_login.html'>ログイン画面へ</a>";
+    print "<a href='../staff_login/staff_login.html'>ログイン画面へ</a>";
 }
 ?>
-
+    
 商品詳細<br><br>
 商品コード<br>
 <?php print $rec["code"];?>
@@ -67,8 +70,11 @@ catch(Exception $e) {
 カテゴリー<br>
 <?php print $rec["category"];?>
 <br><br>
+商品名<br>
+<?php print $rec["name"];?>
+<br><br>
 画像<br>
-<?php if(empty($rec["gaou"]) === true){
+<?php if(empty($rec["gazou"]) === true) {
     $disp_gazou = "";
 } else {
     $disp_gazou = "<img src='./gazou/".$rec['gazou']."'>";
@@ -77,9 +83,9 @@ catch(Exception $e) {
 <br><br>
 詳細<br>
 <?php print $rec["explanation"];?>
-<br><br>
+<br><br>    
 <form>
-    <input type="button" onclick="history.back()" value="戻る">
+<input type="button" onclick="history.back()" value="戻る">
 </form>
 
 </body>
