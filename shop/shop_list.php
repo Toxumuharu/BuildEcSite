e7f164cf92708d7c4b176406d150668e501aab3f
Tokawata <tokawata@microsoft.com>
2022-03-18 14:22:20 +0900

    add update

--- shop/shop_list.php
+++ shop/shop_list.php
@@ -82,7 +82,7 @@ print "<br>";
 }
 catch(Exception $e) {
     print "只今障害が発生しております。<br><br>";
-    print "<a href='../staff/staff_login.html'>ログイン画面へ</a>";
+    print "<a href='../staff/staff_login.php'>ログイン画面へ</a>";
 }
 ?>
 
