1fc2eaa9c60f9483220e64bb9437db61b43e25c4
Tokawata <tokawata@microsoft.com>
2022-03-18 14:24:30 +0900

    add update

--- staff/staff_branch.php
+++ staff/staff_branch.php
@@ -3,7 +3,7 @@ session_start();
 session_regenerate_id(true);
 if(isset($_SESSION["login"]) === false) {
     print "ログインしていません。<br><br>";
-    print "<a href='staff_login.html'>ログイン画面へ</a>";
+    print "<a href='staff_login.php'>ログイン画面へ</a>";
     exit();
 }
 
