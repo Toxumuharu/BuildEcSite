0b9c1ec7435a223369b8873db78e6318c5e81d5e
Tokawata <tokawata@microsoft.com>
2022-03-25 14:32:16 +0900

    add update

--- product/pro_list.php
+++ product/pro_list.php
@@ -1,28 +1,33 @@
-<?php
-
-session_start();
-session_regenerate_id(true);
-if (isset($_SESSION["login"]) === false) {
-    print "ログインしていません。<br><br>";
-    print "<a href='../staff/staff_login.php'>ログイン画面へ</a>";
-    exit();
-} else {
-    print $_SESSION["name"] . "さんログイン中";
-    print "<br><br>";
-}
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>商品一覧</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>商品削除</title>
     <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
+    <?php
+
+    session_start();
+    session_regenerate_id(true);
+    if (isset($_SESSION["login"]) === false) {
+        print "ログインしていません。<br><br>";
+        print "<a href='staff_login.php'>ログイン画面へ</a>";
+        exit();
+    } else {
+        print $_SESSION["name"] . "さんログイン中";
+        print "<p class='text-primary'>.text-primary</p>";
+
+        print "<br><br>";
+    }
+    ?>
 
     <?php
     try {
