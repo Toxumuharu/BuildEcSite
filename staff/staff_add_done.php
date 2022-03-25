1c7f3e412534ef5fdad26eeaf5cd991a97856d30
Tokawata <tokawata@microsoft.com>
2022-03-25 11:18:19 +0900

    add update

--- staff/staff_add_done.php
+++ staff/staff_add_done.php
@@ -46,7 +46,7 @@ if (isset($_SESSION["login"]) === false) {
             $_
         );
 
-        $dsn = "mysql:host={$_["datasource"]};dbname=shop;charset=utf8mb4";
+        $dsn = "mysql:host={$_["datasource"]};dbname=shop;charset=utf8mb4_general_ci";
         $user = $_["userid"];
         $password = $_["password"];
 
